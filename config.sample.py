81347049be1e8935bbf9901d319fb1861a251a72
Joseph Geis <juniorrubyist@gmail.com>
2020-11-03 08:16:12 -0800

    Add sample config

---
+++ config.sample.py
@@ -0,0 +1,11 @@
+# Bot token from Discord panel
+TOKEN = ""
+
+# Command Prefix
+PREFIX = "$"
+
+# Permitted roles
+ELEVATED_ROLES = []
+
+# Verification channel/role pairs
+VERIFICATION_CHANNELS = []
